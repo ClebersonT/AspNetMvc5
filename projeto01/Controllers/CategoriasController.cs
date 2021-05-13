b4c3ef7b16ff2a7e47a13135cdbdc44722e1c380
Cleberson Teodoro <clebersonteodoro2015@hotmail.com>
2021-05-13 16:49:57 -0400

    Inserindo dados em lista via Form - exemplo simples

--- projeto01/Controllers/CategoriasController.cs
+++ projeto01/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using projeto01.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,48 @@ namespace projeto01.Controllers
 {
     public class CategoriasController : Controller
     {
+        public static IList<Categoria> categorias = new List<Categoria>()
+        {
+            new Categoria() {
+                CategoriaId = 1,
+                Nome = "Notebooks"
+            },
+            new Categoria() {
+                CategoriaId = 2,
+                Nome = "Monitores"
+            },
+            new Categoria() {
+                CategoriaId = 3,
+                Nome = "Impressoras"
+            },
+            new Categoria() {
+                CategoriaId = 4,
+                Nome = "Mouses"
+            },
+            new Categoria() {
+                CategoriaId = 5,
+                Nome = "Desktops"
+            }
+        };
         // GET: Categorias
         public ActionResult Index()
         {
+            return View(categorias);
+        }
+
+        public ActionResult Create()
+        {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            categorias.Add(categoria);
+            categoria.CategoriaId = categorias.Select(m => m.CategoriaId).Max() + 1;
+            return RedirectToAction("Index");
+        }
+
     }
 }
