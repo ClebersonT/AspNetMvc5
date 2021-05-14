42463e9118f1cd03539d1490ae5e3c379a9e085b
Cleberson Teodoro <clebersonteodoro2015@hotmail.com>
2021-05-14 16:29:52 -0400

    Adicionando refencias EntityFramework - Adcionando Context

---
+++ projeto01/Contexts/EFContext.cs
@@ -0,0 +1,16 @@
+﻿using projeto01.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace projeto01.Contexts
+{
+    public class EFContext: DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC_CS") { }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+    }
+}
