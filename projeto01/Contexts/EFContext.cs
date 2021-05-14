83eb42de7e16720587fe9b05fc61f8956023af72
Cleberson Teodoro <clebersonteodoro2015@hotmail.com>
2021-05-14 17:25:10 -0400

    Adicionando fabricantes com Entity Framework

--- projeto01/Contexts/EFContext.cs
+++ projeto01/Contexts/EFContext.cs
@@ -1,13 +1,9 @@
 ﻿using projeto01.Models;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Web;
 
 namespace projeto01.Contexts
 {
-    public class EFContext: DbContext
+    public class EFContext : DbContext
     {
         public EFContext() : base("Asp_Net_MVC_CS") { }
         public DbSet<Categoria> Categorias { get; set; }
---
+++ projeto01/Controllers/FabricantesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using projeto01.Contexts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using projeto01.Models;
+
+namespace projeto01.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private EFContext context = new EFContext();
+
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.OrderBy(c => c.Nome));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Fabricante fabricante)
+        {
+            context.Fabricantes.Add(fabricante);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
