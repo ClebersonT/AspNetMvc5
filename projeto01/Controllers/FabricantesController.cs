f8765277ef1cbc1c873152d4e85490f6b56330dc
Cleberson Teodoro <clebersonteodoro2015@hotmail.com>
2021-05-18 13:50:41 -0400

    Incluindo rota editar Fabricantes

--- projeto01/Controllers/FabricantesController.cs
+++ projeto01/Controllers/FabricantesController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using projeto01.Models;
+using System.Net;
 
 namespace projeto01.Controllers
 {
@@ -30,6 +31,38 @@ namespace projeto01.Controllers
             context.Fabricantes.Add(fabricante);
             context.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        //GET: Fabricantes/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Fabricante fabricante = context.Fabricantes.Find(id);
+            if(fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+
+        //POST: Fabricante/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Fabricante fabricante)
+        {
+            //verificando se não há nenhuma validação de erro, como por exemplo um valor exigido porem não preenchido
+            // com a validação do modelo é preciso dizer que o obj sofreu alteração
+            if (ModelState.IsValid)
+            {
+                context.Entry(fabricante).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(fabricante);
         }
     }
 }
